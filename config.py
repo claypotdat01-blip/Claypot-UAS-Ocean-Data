51993a53556fa6f625a7fe09586eacff417ba9b0
Oceana <claypotdat01@gmail.com>
2026-06-17 03:11:04 +0700

    config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
-# ==========================================================
-# config.py
-# ==========================================================
-
 CMEMS_USER = "claypotdat01@gmail.com"
 CMEMS_PASS = "limaJuni_2026"
 
 NASA_USER = "claypotdat1"
 NASA_PASS = "limaJuni_2026"
+
+CDS_UID = ""
 
 CDS_KEY = "e6b04d44-1240-4812-b642-a49e762e49b5"
 
