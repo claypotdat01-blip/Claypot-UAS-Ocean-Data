522265ba4884ca006478890bf860d45327360829
Oceana <claypotdat01@gmail.com>
2026-06-17 03:02:18 +0700

    config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ pip install -r requirements.txt
 
 CMEMS_USER=claypotdat01@gmail.com
 CMEMS_PASS=limaJuni_2026
-NASA_USER=claypotdat01@gmail.com
+NASA_USER=claypotdat1
 NASA_PASS=limaJuni_2026
 CDS_KEY=e6b04d44-1240-4812-b642-a49e762e49b5
 BMKG_ADM4=81.71.01.1001
