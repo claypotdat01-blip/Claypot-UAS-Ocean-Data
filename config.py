4f765013c1a6af63f519757ac4d05426b6aa0c3c
Oceana <claypotdat01@gmail.com>
2026-06-17 03:09:06 +0700

    config.py

--- config.py
+++ config.py
@@ -1,55 +1,18 @@
-"""
-config.py
-"""
-
-import os
-
 # ==========================================================
-# CMEMS
-# ==========================================================
-CMEMS_USER = os.getenv(
-    "CMEMS_USER",
-    "claypotdat01@gmail.com"
-)
-
-CMEMS_PASS = os.getenv(
-    "CMEMS_PASS",
-    "limaJuni_2026"
-)
-
-# ==========================================================
-# NASA EARTHDATA
-# ==========================================================
-NASA_USER = os.getenv(
-    "NASA_USER",
-    "claypotdat01@gmail.com"
-)
-
-NASA_PASS = os.getenv(
-    "NASA_PASS",
-    "limaJuni_2026"
-)
-
-# ==========================================================
-# CDS
+# config.py
 # ==========================================================
 
-# DITAMBAHKAN AGAR IMPORT TIDAK ERROR
-CDS_UID = ""
+CMEMS_USER = "claypotdat01@gmail.com"
+CMEMS_PASS = "limaJuni_2026"
 
-CDS_KEY = os.getenv(
-    "CDS_KEY",
-    "e6b04d44-1240-4812-b642-a49e762e49b5"
-)
+NASA_USER = "claypotdat1"
+NASA_PASS = "limaJuni_2026"
 
-# ==========================================================
-# BMKG
-# ==========================================================
+CDS_KEY = "e6b04d44-1240-4812-b642-a49e762e49b5"
+
+BMKG_ADM4 = "81.71.01.1001"
 BMKG_BASE_URL = "https://inaoc.bmkg.go.id"
 
-# ==========================================================
-# DOMAIN ARAFURA
-# ==========================================================
 LAT_MIN = -12.0
 LAT_MAX = -4.0
 
