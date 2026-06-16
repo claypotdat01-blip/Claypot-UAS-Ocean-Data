badcad1612b327a616a6aff2a2693321691fdda7
Oceana <claypotdat01@gmail.com>
2026-06-17 02:12:26 +0700

    config.py

--- config.py
+++ config.py
@@ -21,20 +21,19 @@ import os
 # ── CMEMS (Copernicus Marine Service) ──────────────────────
 # Daftar: https://marine.copernicus.eu
 # Gratis, approved dalam 1-2 hari kerja
-CMEMS_USER = os.getenv("CMEMS_USER", "ISI_EMAIL_CMEMS_KAMU_DI_SINI")
-CMEMS_PASS = os.getenv("CMEMS_PASS", "ISI_PASSWORD_CMEMS_KAMU_DI_SINI")
+CMEMS_USER = os.getenv("CMEMS_USER", "claypotdat01@gmail.com")
+CMEMS_PASS = os.getenv("CMEMS_PASS", "limaJuni_2026")
 
 # ── NASA Earthdata (MODIS Klorofil-a) ──────────────────────
 # Daftar: https://urs.earthdata.nasa.gov
 # Langsung aktif setelah verifikasi email
-NASA_USER = os.getenv("NASA_USER", "ISI_USERNAME_NASA_EARTHDATA_DI_SINI")
-NASA_PASS = os.getenv("NASA_PASS", "ISI_PASSWORD_NASA_EARTHDATA_DI_SINI")
+NASA_USER = os.getenv("NASA_USER", "claypotdat1")
+NASA_PASS = os.getenv("NASA_PASS", "limaJuni_2026")
 
 # ── CDS / ERA5 (ECMWF Angin) ───────────────────────────────
 # Daftar: https://cds.climate.copernicus.eu
 # Setelah daftar: My Account → API Key → copy UID dan Key
-CDS_UID = os.getenv("CDS_UID", "ISI_UID_CDS_KAMU_DI_SINI")   # angka, misal: 123456
-CDS_KEY = os.getenv("CDS_KEY", "ISI_API_KEY_CDS_KAMU_DI_SINI")  # hex string panjang
+CDS_KEY = os.getenv("CDS_KEY", "e6b04d44-1240-4812-b642-a49e762e49b5")  # hex string panjang
 
 # ── BMKG ───────────────────────────────────────────────────
 # Tidak perlu akun — open data publik
