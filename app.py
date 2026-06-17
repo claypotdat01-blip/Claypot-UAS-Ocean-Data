620671793a723e95167f3f733e692910f1cafa9a
Oceana <claypotdat01@gmail.com>
2026-06-18 01:32:13 +0700

    app.py

--- app.py
+++ app.py
@@ -336,7 +336,7 @@ if st.session_state.page == "home":
     <div style="width:1px;background:#1E3A5C;"></div>
     <div style="text-align:center;"><div style="font-size:32px;font-weight:800;color:#FFFFFF;">3</div><div style="font-size:10px;color:#7BAFD4;font-family:'JetBrains Mono',monospace;margin-top:4px;">SUMBER API</div></div>
     <div style="width:1px;background:#1E3A5C;"></div>
-    <div style="text-align:center;"><div style="font-size:32px;font-weight:800;color:#FFFFFF;">Historis, Real-Time, Proyeksi</div><div style="font-size:8px;color:#7BAFD4;font-family:'JetBrains Mono',monospace;margin-top:4px;">ML FORECAST</div></div>
+    <div style="text-align:center;"><div style="font-size:25px;font-weight:800;color:#FFFFFF;">Historis, Real-Time, Proyeksi</div><div style="font-size:10px;color:#7BAFD4;font-family:'JetBrains Mono',monospace;margin-top:4px;">ML FORECAST</div></div>
   </div>
 </div>
 """, unsafe_allow_html=True)
