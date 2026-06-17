3217d65f30331b8c2611c62a1d481d281eb4805b
Oceana <claypotdat01@gmail.com>
2026-06-17 19:44:59 +0700

    config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
+# config.py
 CMEMS_USER = "claypotdat01@gmail.com"
 CMEMS_PASS = "limaJuni_2026"
 
-NASA_USER = "claypotdat1"
-NASA_PASS = "limaJuni_2026"
+# NASA MODIS — tidak lagi digunakan untuk chla real-time
+NASA_USER = ""
+NASA_PASS = ""
 
-CDS_UID = ""
+CDS_UID   = ""
+CDS_KEY   = "e6b04d44-1240-4812-b642-a49e762e49b5"
 
-CDS_KEY = "e6b04d44-1240-4812-b642-a49e762e49b5"
-
-BMKG_ADM4 = "81.71.01.1001"
+BMKG_ADM4     = "81.71.01.1001"
 BMKG_BASE_URL = "https://inaoc.bmkg.go.id"
 
 LAT_MIN = -12.0
 LAT_MAX = -4.0
-
 LON_MIN = 129.0
 LON_MAX = 144.0
